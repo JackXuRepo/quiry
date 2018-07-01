0ec03f8b6a13f3a0dd88b08286d5c03d9a92e7e6
WeiJian He <kin.he@mail.utoronto.ca>
2018-07-01 15:52:31 -0400

    TEAM11-22 fix package name

---
+++ java/FileStorage/src/file_storage/FileIDSystem.java
@@ -0,0 +1,27 @@
+package file_storage;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class FileIDSystem {
+	public static ArrayList<String> num = new ArrayList<String>();
+	public static String generate() {
+		Random rand = new Random();
+		int n;
+		do {
+			n = rand.nextInt(89999) + 10000;
+		}while(num.indexOf(Integer.toString(n)) != -1);
+		num.add(Integer.toString(n));
+		return Integer.toString(n);
+	}
+	
+	public static void deleteID(String id) {
+		try {
+			num.remove(num.indexOf(id));
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.err.println("ID does not exist.");
+		}
+	}
+
+}
