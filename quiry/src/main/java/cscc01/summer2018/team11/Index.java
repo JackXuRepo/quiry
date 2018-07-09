ec126097f4623a73161ebcb609cb71b824c48652
Wayne Ying <wayne.ying@mail.utoronto.ca>
2018-07-08 20:29:47 -0400

    TEAM11-29 Merge lucene code into quiry

---
+++ quiry/src/main/java/cscc01/summer2018/team11/Index.java
@@ -0,0 +1,80 @@
+package cscc01.summer2018.team11;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+
+public class Index {
+
+    private IndexWriter writer;
+    private Directory index;
+
+    public Index() {
+        // this directory will contain the indexes
+        index = new RAMDirectory();
+
+        // create the index writer
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        try {
+            writer = new IndexWriter(index, config);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    public Directory getIndex() {
+        return index;
+    }
+
+    public void indexFile(FileInfo file) throws IOException {
+        Document document = new Document();
+
+        Field idField = new StringField("id", file.getId(), Field.Store.YES);
+        Field typeField = new StringField("type", file.getFileType(), Field.Store.NO);
+        Field titleField = new StringField("title", file.getTitle(), Field.Store.NO);
+        Field emailField = new StringField("email", file.getAuthorEmail(), Field.Store.NO);
+        Field courseField = new StringField("course", file.getCourse(), Field.Store.NO);
+
+        Field descriptionField = new TextField("description", file.getDescription(), Field.Store.NO);
+        Field contentField = new TextField("content", file.getContent(), Field.Store.NO);
+
+        document.add(idField);
+        document.add(typeField);
+        document.add(titleField);
+        document.add(emailField);
+        document.add(courseField);
+        document.add(descriptionField);
+        document.add(contentField);
+
+        writer.addDocument(document);
+   }
+
+   public int createIndex(List<FileInfo> files) throws IOException {
+       // index every file given list
+       for (FileInfo file : files) {
+           indexFile(file);
+           System.out.println("indexed: " + file.getId() + " " + file.getTitle());
+       }
+       writer.commit();
+       return writer.numDocs();
+   }
+
+}
---
+++ quiry/src/test/java/cscc01/summer2018/team11/Lucene.java
@@ -0,0 +1,125 @@
+package cscc01.summer2018.team11;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
+
+/**
+ * Test Index and Search.
+ */
+public class Lucene {
+
+    private static Index index = new Index();
+    private static List<FileInfo> fileStorage = new ArrayList<>();
+
+    private static FileInfo doc1() {
+        String email = "doc1@example.com";
+        String title = "doc1";
+        String description = "test doc1";
+        String course = "cscc01";
+        int access = 0;
+        String type = "txt";
+
+        String content = "Ever man are put down his very. And marry may table him avoid. Hard " +
+                "sell it were into it upon. He forbade affixed parties of assured to me " +
+                "windows. Happiness him nor she disposing provision. Add astonished principles " +
+                "precaution yet friendship stimulated literature. State thing might stand one " +
+                "his plate. Offending or extremity therefore so difficult he on provision. " +
+                "Tended depart turned not are. ";
+
+        return new FileInfo(email, title, description, course, access, type, content);
+    }
+
+    private static FileInfo doc2() {
+        String email = "doc2@abc.com";
+        String title = "doc2";
+        String description = "test doc2";
+        String course = "cscc01";
+        int access = 1;
+        String type = "html";
+
+        String content = "Led ask possible mistress relation elegance eat likewise debating. By " +
+                "message or am nothing amongst chiefly address. The its enable direct men depend " +
+                "highly. Ham windows sixteen who inquiry fortune demands. Is be upon sang fond " +
+                "must shew. Really boy law county she unable her sister. Feet you off me like " +
+                "like six. Among sex are leave law built now. In built table in an rapid blush. " +
+                "Merits behind on afraid or warmly. ";
+
+        return new FileInfo(email, title, description, course, access, type, content);
+    }
+
+    private static FileInfo doc3() {
+        String email = "doc3@xyz.com";
+        String title = "doc3";
+        String description = "test doc3";
+        String course = "cscc01";
+        int access = 2;
+        String type = "pdf";
+
+        String content = "Article evident arrived express highest men did boy. Mistress sensible " +
+                "entirely am so. Quick can manor smart money hopes worth too. Comfort produce " +
+                "husband boy her had hearing. Law others theirs passed but wishes. You day real " +
+                "less till dear read. Considered use me dispatched melancholy sympathize discretion " +
+                "led. Oh feel if up to till like. ";
+
+        return new FileInfo(email, title, description, course, access, type, content);
+    }
+
+    public static void main(String[] args) {
+        // test with 3 documents
+        fileStorage.add( doc1() );
+        fileStorage.add( doc2() );
+        fileStorage.add( doc3() );
+
+        index();
+        search();
+    }
+
+    public static void index() {
+        try {
+            index.createIndex(fileStorage);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void search() {
+        Search search = new Search(index.getIndex());
+
+        // content: me, she, like
+        try {
+            TopDocs hits = search.search("content", "like");
+            System.out.println("found: " + hits.scoreDocs.length);
+
+            for (ScoreDoc scoreDoc : hits.scoreDocs) {
+                Document doc = search.getDocument(scoreDoc);
+
+                // lookup id in FileStorage to locate actual file
+                FileInfo file = getFile(doc.get("id"));
+                System.out.println(file.getTitle());
+            }
+        } catch (ParseException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static FileInfo getFile(String id) {
+        // use hash map or database lookup
+        for (FileInfo file : fileStorage) {
+            if (file.getId().equals(id)) {
+                return file;
+            }
+        }
+        return null;
+    }
+
+}
