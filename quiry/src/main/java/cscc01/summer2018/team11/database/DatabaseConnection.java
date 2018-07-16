c912a822947aa28882016c1ab3a3f48727556767
Wayne Ying <wayne.ying@mail.utoronto.ca>
2018-07-15 23:42:50 -0400

    TEAM11-37 Fix import and indentation

--- quiry/src/main/java/cscc01/summer2018/team11/database/DatabaseConnection.java
+++ quiry/src/main/java/cscc01/summer2018/team11/database/DatabaseConnection.java
@@ -1,76 +1,72 @@
 package cscc01.summer2018.team11.database;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
 public class DatabaseConnection {
-	
-	private Connection dbConnection;
 
-	
-	public DatabaseConnection() throws SQLException {
-		this.dbConnection = connectToDatabase("quiry");
-		createNewUserTable();
-		createNewFileTable();
+    private Connection dbConnection;
 
-	}
-	
-	
-	public Connection connectToDatabase(String dbName) {
-		String url = "jdbc:sqlite:C:" + dbName +".db";
-    	Connection conn = null;
+
+    public DatabaseConnection() throws SQLException {
+        this.dbConnection = connectToDatabase("quiry");
+        createNewUserTable();
+        createNewFileTable();
+    }
+
+
+    public Connection connectToDatabase(String dbName) {
+        String url = "jdbc:sqlite:C:" + dbName +".db";
+        Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return conn;
-	}
-	
-	
+    }
+
+
     public void createNewUserTable() throws SQLException {
-    	// for now lets create the local database in the home directory
+        // for now lets create the local database in the home directory
         String sql = "CREATE TABLE IF NOT EXISTS User (\n"
-             	+ "	userId text PRIMARY KEY,\n"
-             	+ "	firstName text NOT NULL,\n"
-             	+ "	lastName text NOT NULL,\n"
-             	+ "	email text NOT NULL,\n"
-             	+ "	password text NOT NULL,\n"
-             	+ "	accesslvl int NOT NULL,\n"
-             	+ ");";
- 
+                + "userId text PRIMARY KEY,\n"
+                + "firstName text NOT NULL,\n"
+                + "lastName text NOT NULL,\n"
+                + "email text NOT NULL,\n"
+                + "password text NOT NULL,\n"
+                + "accesslvl int NOT NULL,\n"
+                + ");";
 
         dbConnection.createStatement().execute(sql);
-
     }
-    
+
     public void createNewFileTable() throws SQLException {
-    	// for now lets create the local database in the home directory
+        // for now lets create the local database in the home directory
         String sql = "CREATE TABLE IF NOT EXISTS File (\n"
-             	+ "	fileId int PRIMARY KEY,\n"
-             	+ "	userId text NOT NULL,\n"
-             	+ "	fileType int NOT NULL,\n"
-             	+ "	contentType int NOT NULL,\n"
-             	+ "	accesslvl int NOT NULL,\n"
-             	+ "	title text NOT NULL,\n"
-             	+ "	course text NOT NULL,\n"
-             	+ "	courseRestricted int NOT NULL,\n"
-             	+ "	filePath text NOT NULL,\n"
-             	+ "	description text NOT NULL,\n"
-             	+ " FOREIGN KEY (userId) REFERENCES User (userId)\n"
-             	+ " ON DELETE CASCADE ON UPDATE NO ACTION"
-             	+ ");";
-        
+                + "fileId int PRIMARY KEY\n,"
+                + "userId text NOT NULL,\n"
+                + "fileType int NOT NULL,\n"
+                + "contentType int NOT NULL,\n"
+                + "accesslvl int NOT NULL,\n"
+                + "title text NOT NULL,\n"
+                + "course text NOT NULL,\n"
+                + "courseRestricted int NOT NULL,\n"
+                + "filePath text NOT NULL,\n"
+                + "description text NOT NULL,\n"
+                + "FOREIGN KEY (userId) REFERENCES User (userId)\n"
+                + "ON DELETE CASCADE ON UPDATE NO ACTION\n"
+                + ");";
 
         dbConnection.createStatement().execute(sql);
-
     }
-    
+
 
     public Connection getConnection() {
-    	return this.dbConnection;
+        return this.dbConnection;
     }
 
 }
--- quiry/src/main/java/cscc01/summer2018/team11/database/FileDAO.java
+++ quiry/src/main/java/cscc01/summer2018/team11/database/FileDAO.java
@@ -1,5 +1,6 @@
 package cscc01.summer2018.team11.database;
 
+
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.sql.Connection;
@@ -7,114 +8,110 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class FileDAO{
-     Connection c;
-     public Connection getC() {
-          return c;
-     }
-
-     public void setC(Connection c) {
-          this.c = c;
-     }
-
-     public Statement getStmt() {
-          return stmt;
-     }
-
-     public void setStmt(Statement stmt) {
-          this.stmt = stmt;
-     }
+import cscc01.summer2018.team11.file.FileInfo3;
 
 
-     Statement stmt;
+public class FileDAO {
 
-     
-     
-     public FileDAO(Connection c) throws SQLException{
-          this.c = c;
-          this.stmt = c.createStatement();
+    private Connection c;
+    private Statement stmt;
 
-     }
-     
-     public void addFile(FileInfo3 fileData) throws SQLException{
-          String sql = "INSERT INTO File (fileId, userId, fileType, contentType, accesslvl, title, course, courseRestricted, filePath, description) "
-                 + "VALUES (?,?,?,?,?,?,?,?,?,?);";
-         PreparedStatement stmt = c.prepareStatement(sql);
-         int i = 0;
-         stmt.setInt(i++, fileData.getFileId());
-         stmt.setString(i++, fileData.getUserId()); // Need to add userId to to FileInfo
-         stmt.setInt(i++, fileData.getFileType());
-         stmt.setInt(i++, fileData.getContentType()); // Need to add content type variable to FileInfo
-         stmt.setInt(i++, fileData.getAccessLevel());
-         stmt.setString(i++, fileData.getTitle());
-         stmt.setString(i++, fileData.getCourse());
-         stmt.setBoolean(i++, fileData.isCourseRestricted()); // Need to add course restricted variable to FileInfo
-         stmt.setString(i++, fileData.getFilePath()); // Need to add file path variable to FileInfo
-         stmt.setString(i++, fileData.getDescription());
-         
-         stmt.executeUpdate(sql);
-     }
-     
-     public void deleteFile(int fileId){
-         String sql = "DROP * FROM INVOICE WHERE fileId=" + fileId + ";";
-         stmt.executeUpdate(sql);
-     }
-     
-     public void close() throws SQLException{
-          stmt.close();
-     }
-     
-     public FileInfo3 getFileByFileId(String fileId) throws SQLException{
+    public FileDAO(Connection c) throws SQLException {
+        this.c = c;
+        this.stmt = c.createStatement();
+    }
+
+    public Connection getC() {
+        return c;
+    }
+
+    public void setC(Connection c) {
+        this.c = c;
+    }
+
+    public Statement getStmt() {
+        return stmt;
+    }
+
+    public void setStmt(Statement stmt) {
+        this.stmt = stmt;
+    }
+
+    public void addFile(FileInfo3 fileData) throws SQLException{
+        String sql = "INSERT INTO File (fileId, userId, fileType, contentType, accesslvl, title, course, courseRestricted, filePath, description) "
+                + "VALUES (?,?,?,?,?,?,?,?,?,?);";
+        PreparedStatement stmt = c.prepareStatement(sql);
+
+        int i = 0;
+        stmt.setInt(i++, fileData.getFileId());
+        stmt.setString(i++, fileData.getUserId()); // Need to add userId to to FileInfo
+        stmt.setInt(i++, fileData.getFileType());
+        stmt.setInt(i++, fileData.getContentType()); // Need to add content type variable to FileInfo
+        stmt.setInt(i++, fileData.getAccessLevel());
+        stmt.setString(i++, fileData.getTitle());
+        stmt.setString(i++, fileData.getCourse());
+        stmt.setBoolean(i++, fileData.isCourseRestricted()); // Need to add course restricted variable to FileInfo
+        stmt.setString(i++, fileData.getFilePath()); // Need to add file path variable to FileInfo
+        stmt.setString(i++, fileData.getDescription());
+
+        stmt.executeUpdate(sql);
+    }
+
+    public void deleteFile(int fileId) throws SQLException {
+        String sql = "DROP * FROM INVOICE WHERE fileId=" + fileId + ";";
+        stmt.executeUpdate(sql);
+    }
+
+    public void close() throws SQLException{
+         stmt.close();
+    }
+
+    public FileInfo3 getFileByFileId(String fileId) throws SQLException{
         ResultSet rs = stmt.executeQuery( "SELECT * FROM INVOICE WHERE fileId=" + fileId + ";");
-        
+
         rs.next();
         int i = 0;
-		
+
         int id = rs.getInt(i++);
-	    String userId= rs.getString(i++);
-		int fileType = rs.getInt(i++);
-		int contentType = rs.getInt(i++);
-		int accessLevel = rs.getInt(i++);
-		String title = rs.getString(i++);
-		String course = rs.getString(i++);
-		boolean courseRestricted = rs.getBoolean(i++);
-		String filePath = rs.getString(i++);
-		String description = rs.getString(i++);
-		
-		FileInfo3 fileData = new FileInfo3(id, userId, title, description, course,
-				accessLevel, fileType, contentType, filePath, courseRestricted);
-		
-        
+        String userId= rs.getString(i++);
+        int fileType = rs.getInt(i++);
+        int contentType = rs.getInt(i++);
+        int accessLevel = rs.getInt(i++);
+        String title = rs.getString(i++);
+        String course = rs.getString(i++);
+        boolean courseRestricted = rs.getBoolean(i++);
+        String filePath = rs.getString(i++);
+        String description = rs.getString(i++);
+
+        FileInfo3 fileData = new FileInfo3(id, userId, title, description, course,
+                accessLevel, fileType, contentType, filePath, courseRestricted);
+
         return fileData;
-     }
-     
-     public ArrayList<FileInfo3> getFilesByUserId(String idUser) throws SQLException{
-         ResultSet rs = stmt.executeQuery( "SELECT * FROM INVOICE WHERE userId=" + idUser + ";");
-         ArrayList<FileInfo3> fileDataList = new ArrayList<FileInfo3>();
-         
-         while(rs.next()) {
-        	 int i = 0;
-             int id = rs.getInt(i++);
-     	     String userId= rs.getString(i++);
-     		 int fileType = rs.getInt(i++);
-     		 int contentType = rs.getInt(i++);
-     		 int accessLevel = rs.getInt(i++);
-     		 String title = rs.getString(i++);
-     		 String course = rs.getString(i++);
-     		 boolean courseRestricted = rs.getBoolean(i++);
-     		 String filePath = rs.getString(i++);
-     		 String description = rs.getString(i++);
-     		 
-     		 FileInfo3 fileData = new FileInfo3(id, userId, title, description, course,
-    				accessLevel, fileType, contentType, filePath, courseRestricted);
-     		 fileDataList.add(fileData);
-     		 
-     		 
-         }
-         
-         return fileDataList;
-      }
-      
-     
+    }
+
+    public ArrayList<FileInfo3> getFilesByUserId(String idUser) throws SQLException{
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM INVOICE WHERE userId=" + idUser + ";");
+        ArrayList<FileInfo3> fileDataList = new ArrayList<FileInfo3>();
+
+        while(rs.next()) {
+            int i = 0;
+            int id = rs.getInt(i++);
+            String userId= rs.getString(i++);
+            int fileType = rs.getInt(i++);
+            int contentType = rs.getInt(i++);
+            int accessLevel = rs.getInt(i++);
+            String title = rs.getString(i++);
+            String course = rs.getString(i++);
+            boolean courseRestricted = rs.getBoolean(i++);
+            String filePath = rs.getString(i++);
+            String description = rs.getString(i++);
+
+            FileInfo3 fileData = new FileInfo3(id, userId, title, description, course,
+                    accessLevel, fileType, contentType, filePath, courseRestricted);
+            fileDataList.add(fileData);
+        }
+
+        return fileDataList;
+    }
 
 }
