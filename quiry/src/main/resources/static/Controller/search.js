dfba1dcf90f55319c5a812cd0e2225a42106e1ff
Haosen Xu <haosen.xu@mail.utoronto.ca>
2018-07-28 15:17:53 -0400

    TEAM11-43 Fixed some issues for the search ui

--- quiry/src/main/resources/static/Controller/search.js
+++ quiry/src/main/resources/static/Controller/search.js
@@ -6,7 +6,7 @@
 		.controller('searchController', searchController);
 		
 		function searchController($scope, $http, StorageService, $window){
-			$scope.searchText = "";
+			$scope.searchText = StorageService.getValue("searchText");
 			$scope.author = "";
 			$scope.timeOptions = ["Today", "This Week", "This Month", "This Year", "Anytime"];
 			$scope.advancedSearchOn = false;
@@ -30,36 +30,37 @@
 				StorageService.removeValue("results");
 				//console.log(parseCourseJson($scope.coursesSelected));
 
-				// $http.get("../json/mock.json")
-				// 	.then(function(response){
-				// 		StorageService.setValue("results", response.data);
-				// 		$window.location.href = "./results.html";
-				// 	});
-				var paramConfig = {params: {   
-									searchText: $scope.searchText,
-									author: $scope.author,
-						    		dateUploaded: parseTimeOptions($scope.dateUploaded),
-									instructorSearch: $scope.instructorSearch,
-									studentSearch: $scope.studentSearch,
-									pastExams: $scope.pastExams,
-									notes: $scope.notes,
-									journals: $scope.journals,
-									pdfType: $scope.pdfType,
-									txtType: $scope.txtType,
-									htmlType: $scope.htmlType,
-									courses: parseCourseObject($scope.coursesSelected)
-				    			}};
-				console.log(paramConfig.params);
-				$http.get( "/search/advancedSearch", paramConfig)
+				$http.get("../json/mock.json")
 					.then(function(response){
-					 	console.log(response.data);
-					 	StorageService.setValue("results", response.data);
-					 	$window.location.href = "./results.html";
-					 })
-					.catch(function(response){
-						console.log(response.data);
-					 	//$window.location.href = "./results.html";
+						StorageService.setValue("results", response.data);
+						StorageService.setValue("searchText", $scope.searchText);
+						$window.location.href = "./results.html";
 					});
+				// var paramConfig = {params: {   
+				// 					searchText: $scope.searchText,
+				// 					author: $scope.author,
+				// 		    		dateUploaded: parseTimeOptions($scope.dateUploaded),
+				// 					instructorSearch: $scope.instructorSearch,
+				// 					studentSearch: $scope.studentSearch,
+				// 					pastExams: $scope.pastExams,
+				// 					notes: $scope.notes,
+				// 					journals: $scope.journals,
+				// 					pdfType: $scope.pdfType,
+				// 					txtType: $scope.txtType,
+				// 					htmlType: $scope.htmlType,
+				// 					courses: parseCourseObject($scope.coursesSelected)
+				//     			}};
+				// console.log(paramConfig.params);
+				// $http.get( "/search/advancedSearch", paramConfig)
+				// 	.then(function(response){
+				// 	 	console.log(response.data);
+				// 	 	StorageService.setValue("results", response.data);
+				// 	 	$window.location.href = "./results.html";
+				// 	 })
+				// 	.catch(function(response){
+				// 		console.log(response.data);
+				// 	 	//$window.location.href = "./results.html";
+				// 	});
 			}
 
 			$scope.resetAdvancedSearch = function(){
@@ -96,8 +97,14 @@
 			}
 
 			$scope.loadOptions = function(query, path) {
-    			return $http.get(path);
+    			return $http.get(path, { cache: true}).then(function(response) {
+			      var courses = response.data;
+			      return courses.filter(function(course) {
+			        return course.text.toLowerCase().indexOf(query.toLowerCase()) != -1;
+      				});
+    			});
   			}
+
 
 			$scope.toggleAdvancedSearch = function(){
 				$scope.advancedSearchOn = !$scope.advancedSearchOn;
